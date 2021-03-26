a4607bfeda0545daab0104fb232b551a7dfe359c
mrevanishere <mrevanishere@gmail.com>
2021-03-25 17:02:16 -0700

    binary shifting question and binary search

---
+++ Problems/LeetCode/LeetCodeTemplate.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+	void placeHolderFunction(int 1) {
+		return;
+	}
+};
+
+int main() {
+
+	Solution sol = Solution();
+
+	int input = 1;
+
+	std::cout << "Input: " << std::endl;
+	std::cout << "Output: " << placeHolderFunction(input);
+
+	return 0;
+}
